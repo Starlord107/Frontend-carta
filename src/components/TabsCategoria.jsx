99168a6aaa9888593e82db566a450bd9d0688858
Starlord107 <alex7rodrivela10@gmail.com>
2026-03-11 15:37:07 +0100

    fix change size icons

--- src/components/TabsCategoria.jsx
+++ src/components/TabsCategoria.jsx
@@ -104,7 +104,17 @@ export default function CategoriasTabs({
               >
                 <img
                   src={iconSrc}
-                  className={`object-contain ${sub === "Platos" ? "w-8 h-8" : "w-6 h-6"}`}
+                  className={`object-contain ${sub === "Platos" ? "w-10 h-10" : "w-10 h-10"}`}
+                  alt={sub}
+                />
+                <img
+                  src={iconSrc}
+                  className={`object-contain ${sub === "Postres" ? "w-10 h-10" : "w-10 h-10"}`}
+                  alt={sub}
+                />
+                <img
+                  src={iconSrc}
+                  className={`object-contain ${sub === "Bocadillos" ? "w-8 h-8" : "w-6 h-6"} p-2`}
                   alt={sub}
                 />
                 <span className="truncate">{sub}</span>
