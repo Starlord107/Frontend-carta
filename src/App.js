07ef6678ba8d94d0dba227497052f21f4ff2b6b5
Starlord107 <alex7rodrivela10@gmail.com>
2026-03-11 14:45:02 +0100

    fix modify cosas de cliente carta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // RUTAS CAMARERO
-import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";      
 
+import Carta from "./pages/cliente/CartaCliente";
 
-// RUTA CLIENTE
-import CartaCliente from "./pages/cliente/CartaCliente";
-
-// PROTECCIÓN DE RUTAS
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route path="/" element={<Navigate to="/cliente/carta" replace/>}/>
 
-        {/* LOGIN CAMARERO */}
 
-        {/* RUTAS PROTEGIDAS CAMARERO */}
+      <Route path="/cliente/CartaCliente" element={<Carta />} />
        
 
         
