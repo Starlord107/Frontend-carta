664ee7a9871994b6bc71c398bcb88fb42d4730c6
Starlord107 <alex7rodrivela10@gmail.com>
2026-03-11 14:01:22 +0100

    fix changes tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // RUTAS CAMARERO
-import SeleccionarMesa from "./pages/camarero/SeleccionarMesa";
-import Carta from "./pages/camarero/Carta";
-import LoginCamarero from "./pages/camarero/LoginCamarero";
-import AdminProductos from "./pages/camarero/AdminProductos";
 import { Navigate } from "react-router-dom";
 
 
