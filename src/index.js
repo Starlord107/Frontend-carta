af66d18d0b8c03ef3ba64d80977f12e74498523b
Starlord107 <alex7rodrivela10@gmail.com>
2026-03-11 13:58:27 +0100

    fix changes tabs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HeroUIProvider } from "@heroui/react";  
-import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
